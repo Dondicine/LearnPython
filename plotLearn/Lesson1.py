28fb766887d70959e5e2b86702a286b9333fc1ec
mac <mac@macdeMac-mini.local>
2020-10-30 12:12:25 +0800

    10/30

--- plotLearn/Lesson1.py
+++ plotLearn/Lesson1.py
@@ -2,38 +2,44 @@ import numpy as np
 
 # method 1
 
-# arr1 = np.zeros(5)
+arr1 = np.zeros(5)
 
-# arr2 = np.zeros([5,3],dtype=int)
+arr2 = np.zeros([5,3],dtype=int)
 
-# print("arr1 = ", arr1)
+print("arr1 = ", arr1)
 
-# print("arr2 = ", arr2)
+print("arr2 = ", arr2)
 
 # method 2
 
-# arr1 = np.array([0,1,2])
+arr1 = np.array([0,1,2])
 
-# arr2 = np.array([[0, 1, 2],
-# [3, 2, 1],
-# [-1, -2, -3],
-# [0, 0, 0]])
+arr2 = np.array([[0, 1, 2],
+[3, 2, 1],
+[-1, -2, -3],
+[0, 0, 0]])
 
-# print("arr1 = ", arr1)
+print("arr1 = ", arr1)
 
-# print("arr2 = ")
-# print(arr2)
+print("arr2 = ")
+print(arr2)
 
-# print(arr2.shape, arr2.shape[0], arr2.shape[1])
+print(arr2.shape, arr2.shape[0], arr2.shape[1])
 
 
 # method 3
 
-# arr1 = np.arange(0, 10.001, 0.5)
-# print(arr1)
+arr1 = np.arange(0, 10.001, 0.5)
+print(arr1)
 
 # mehod 4
 
 arr1 = np.linspace(0, 10, 11, endpoint=False)
 
 print(arr1)
+
+# numpy array can be of more types
+
+arr = np.array(["name",1,2,5.0],dtype=object)
+
+print(arr)
